472eba00ced13a184a0d01bb2275543e49512023
Elí Maciel <tecno_eli@hotmail.com>
2022-01-18 11:58:11 -0600

    add a avatar command

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,22 @@
 import { config } from "dotenv";
 config();
 
-import { Client } from "revolt.js";
+import { Client } from "./classes/Client";
 import { Message } from "revolt.js/dist/maps/Messages";
+
 
 let client = new Client();
 
 client.on("ready", async () => {
-    console.info(`${client.user!.username} Listo y Atento!!!`);    
+    console.info(`${client.user!.username} Listo y Atento!!!`);
 });
 
-client.on('message', (message: Message) => {
-    if (String(message.content).startsWith('>')) {
-        const args = String(message.content).slice(1).split(/ /gi)
-        const command = args.shift()
-        console.log(command);
-        if (command == 'avatar') {
-            message.reply(`[Your avatar](${message.author?.generateAvatarURL()})`)
-        }
+client.on("message", async (message: Message) => {
+    if (String(message.content).startsWith(">")) {
+        const args = String(message.content).slice(1).split(/ /gi);
+        const command = args.shift();
+        client.commands.find(c=>c.name === command?.toLowerCase() || c.alias.includes(command?.toLowerCase() as string))?.run(message, args)
     }
-})
+});
 
 client.loginBot(process.env.REVOLT_TOKEN as string);
-
